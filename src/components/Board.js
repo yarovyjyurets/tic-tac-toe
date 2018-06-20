7d73443171361d21451f912dac77ca6ebf553319
Yurets Yarovyi <Yurii_Yarovyi@epam.com>
2018-06-20 17:30:05 +0300

    add winner check

--- src/components/Board.js
+++ src/components/Board.js
@@ -24,15 +24,48 @@ class Board extends Component {
     }
   }
 
+  calculateWinner = () => {
+    const { squares } = this.state;
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  satrtnewGame = () => {
+    this.setState({
+      squares: Array(9).fill(null),
+      nextTurnIsX: true
+    });
+  }
   render() {
     const { squares, nextTurnIsX } = this.state;
 
-    return (
+    const winner = this.calculateWinner();
+    console.log('>>>>>', winner)
+    return winner ?
+      <div>
+        <h2>WINNER is : "{winner}"</h2>
+        <button onClick={this.satrtnewGame}>Start new game!!!</button>
+      </div> :
       <div>
         <GameInfo nextPlayerTurn={nextTurnIsX ? 'X' : 'O'} />
-        <Playground squares={squares} updateSquares={this.handleSquareClick}/>
+        <Playground squares={squares} updateSquares={this.handleSquareClick} />
       </div>
-    );
+
   }
 }
 
